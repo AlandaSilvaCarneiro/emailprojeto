79fb31ecda49249f21412317df7d994dc71f5b89
AlandaSilvaCarneiro <82735697+AlandaSilvaCarneiro@users.noreply.github.com>
2024-03-05 08:21:05 -0300

    Init

---
+++ src/main/java/br/com/java/alan/emailprojeto/emailprojeto/EmailprojetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.alan.emailprojeto.emailprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/alan/emailprojeto/emailprojeto/EmailprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.alan.emailprojeto.emailprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
